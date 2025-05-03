f191cb3a0a8a3e37d0b85eabbb46bf29d93d843c
Dipok Kumar Ray <dipok7316@gmail.com>
2025-05-03 09:30:27 +0600

    Navbar design complete

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Header from '../Components/Header';
 import LatestNews from '../Components/LatestNews';
+import Navbar from '../Components/Navbar';
 
 const HomeLayout = () => {
     return (
@@ -13,6 +14,9 @@ const HomeLayout = () => {
               
                 </LatestNews>
             </section>
+            <nav className='w-11/12 mx-auto'>
+                <Navbar  />
+            </nav>
             </header>
             <main>
                 <section className="left_nav"></section>
